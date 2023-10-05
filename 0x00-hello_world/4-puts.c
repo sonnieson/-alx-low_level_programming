1c81be7f62c38b979a6a55260bebcf0b045986d8
sonnieson <mutiriaesther9@gmail.com>
2023-10-05 19:48:37 +0300

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#!/bin/bash
 #include<stdio.h>
-/**
- * main - Entry point
+/*
+ * main - A program that prints a line with puts function
  *
- * Return:Always 0 (Success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+		
 }	
