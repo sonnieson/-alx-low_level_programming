fbfc3074f2d79a9a40ed9b232edd3f2392a259be
sonnieson <mutiriaesther9@gmail.com>
2023-10-23 22:05:03 +0300

    String

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,28 +1,10 @@
-#include <stdio.h>
 #include "main.h"
-
+/**
+ * set-string - sets the value of a pointer to a char
+ * @s: pointer to pointer
+ * @to: pointer char
+ */
 void set_string(char **s, char *to)
 {
-	if (*s != NULL)
-	{
-		free(*s);
-	}
-	*s = (char *)malloc(strlen(to) + 1);
-	
-	if (*s != NULL){
-		strcpy(*s, to);
-		}
-}
-int main() {
-	char *str = NULL;
-	char *newStr = "Hello, World!";
-
-	set_string(&str, newStr);
-
-	if (str != NULL)
-	{
-		printf("str: %s\n", str);
-		free(str);
-	}
-	return 0;
+	*s = to;
 }
